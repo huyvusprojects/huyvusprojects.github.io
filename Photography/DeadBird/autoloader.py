88050d7dd54a19ad06f8850244a7ef8f5d87c48e
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2020-11-27 22:21:51 +0700

    20201127 1

---
+++ Photography/DeadBird/autoloader.py
@@ -0,0 +1,29 @@
+import os
+
+def append(num):
+    front = "				<img src=\"images/"
+    back = ".jpg\" onContextMenu=\"return false;\"/>\n"
+    string = front + str(num) + back
+    return string
+
+def autoload(num):
+	mid = ""
+	for i in range(1,num+1):
+		mid += append(i)
+	return mid
+def count_files():
+    return len([name for name in os.listdir('images') if os.path.isfile('images/' + name)])
+def main():
+	filecount = count_files()
+	with open('front.txt') as f:
+		start = f.readlines()
+	start = "".join(start)
+	with open('back.txt') as f:
+		end = f.readlines()
+	end = "".join(end)
+	mid = autoload(filecount)
+	return start + mid + end
+
+file = open("SilverCollection.html","w")
+file.write(main())
+file.close()
