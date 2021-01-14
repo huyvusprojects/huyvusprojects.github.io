7a2518f967705d5367866138d178cc9f76040b0b
Solar-Flare <untitled173a@gmail.com>
2021-01-14 17:31:30 +0700

    ..auto fix v5?

--- Photography/html.py
+++ Photography/html.py
@@ -5,15 +5,6 @@ def write_html(link):
     index = links.count("/")
     html_text = ""
     html_text += get_text(os.path.dirname(os.path.realpath(__file__)) + "/front.txt", index + 1)
-    """
-    lines = get_title_text(link)
-    if lines == None:
-        pass
-    else:
-        html_text += "        <h2 class=\"notes\">"
-        html_text += lines
-        html_text += "</h2>\n"
-    """
     html_text += array_wrapper(link)
     html_text += get_text(os.path.dirname(os.path.realpath(__file__)) + "/back.txt", index + 1)
     return html_text
@@ -35,13 +26,16 @@ def get_title_text(link):
 """
 
 
-def get_text(link, value):
+def get_text(link, value=False):
     # link: either front.txt or back.txt
     text = ""
     with open(link) as f:
         text = f.readlines()
     text = "".join(text)
-    return text.replace("../../", "../" * value)
+    if value == False:
+        return text
+    else:
+        return text.replace("../../", "../" * value)
 
 
 def get_image_array(link):
@@ -62,6 +56,6 @@ def array_wrapper(link):
 #print(get_text(os.path.dirname(os.path.realpath(__file__)) + "/front.txt"))
 #print(os.path.dirname(os.path.realpath(__file__)))
 #print(get_title_text(os.path.dirname(os.path.realpath(__file__)) + "\\Toaster"))
-#print(write_html(os.path.dirname(os.path.realpath(__file__)) + "/"))
+print(write_html(os.path.dirname(os.path.realpath(__file__)) + "/LadanTet"))
 #print(write_html())
 #print(get_image_count(os.path.dirname(os.path.realpath(__file__)) + "\\Potato\\Potatoe"))
