54cb47770d09ff4351391e1c67db48296ccdbdca
Solar-Flare <untitled173a@gmail.com>
2021-01-14 15:19:57 +0700

    autoloader fix

--- Photography/autoloader.py
+++ Photography/autoloader.py
@@ -7,16 +7,16 @@ def get_directories(link):
     for address in os.listdir(link):
         if address == "images":
             all_directories.append(link)
-        elif os.path.isdir(link + "\\" + address):
-            to_check.append(link + "\\" + address)
+        elif os.path.isdir(link + "/" + address):
+            to_check.append(link + "/" + address)
     while len(to_check) > 0:
         link = to_check[0]
         to_check.pop(0)
         for address in os.listdir(link):
             if address == "images":
                 all_directories.append(link)
-            elif os.path.isdir(link + "\\" + address):
-                to_check.append(link + "\\" + address)
+            elif os.path.isdir(link + "/" + address):
+                to_check.append(link + "/" + address)
     return all_directories
     #for os.path.dirname(os.path.realpath(__file__))
 
@@ -27,11 +27,11 @@ def main():
             array_list.append(folder)
     all_directories = []
     for address in array_list:
-        directory = get_directories(os.path.dirname(os.path.realpath(__file__)) + "\\" + address)
+        directory = get_directories(os.path.dirname(os.path.realpath(__file__)) + "/" + address)
         for dir in directory:
             all_directories.append(dir)
     for address in all_directories:
-        file = open(address + "\\index.html", "w")
+        file = open(address + "/index.html", "w")
         file.write(html.write_html(address))
         file.close()
 
--- Photography/html.py
+++ Photography/html.py
@@ -1,8 +1,11 @@
 import os
 def write_html(link):
-    print(link)
+    positionLength = len(os.path.dirname(os.path.realpath(__file__)))
+    links = link[positionLength:]
+    index = links.count("/")
     html_text = ""
-    html_text += "".join(get_text("front.txt"))
+    html_text += get_text(os.path.dirname(os.path.realpath(__file__)) + "/front.txt", index + 1)
+    """
     lines = get_title_text(link)
     if lines == None:
         pass
@@ -10,35 +13,40 @@ def write_html(link):
         html_text += "        <h2 class=\"notes\">"
         html_text += lines
         html_text += "</h2>\n"
+    """
     html_text += array_wrapper(link)
-    html_text += "".join(get_text("back.txt"))
+    html_text += get_text(os.path.dirname(os.path.realpath(__file__)) + "/back.txt", index + 1)
     return html_text
 
 
+"""
+rip
 def get_title_text(link):
-    if os.path.isfile(link + "\\title.txt"):
+    if os.path.isfile(link + "/title.txt"):
         title_text = ""
-        file = open(link + "\\title.txt")
+        file = open(link + "/title.txt")
         lines = file.readlines()
         for line in lines:
             title_text += line
         file.close()
-
         return title_text.replace("\n", "<br>")
     else:
         return None
+"""
 
-def get_text(link):
+
+def get_text(link, value):
     # link: either front.txt or back.txt
     text = ""
     with open(link) as f:
         text = f.readlines()
-    return text
+    text = "".join(text)
+    return text.replace("../../", "../" * value)
 
 
 def get_image_array(link):
     # link: Address to the folder of the operation
-    dir = link + "\\images"
+    dir = link + "/images"
     return [name for name in os.listdir(dir) if name != '.DS_Store']
 
 
@@ -51,8 +59,9 @@ def array_wrapper(link):
         text += image
         text += "\" onContextMenu=\"return false;\"/>\n"
     return text
-
+#print(get_text(os.path.dirname(os.path.realpath(__file__)) + "/front.txt"))
+#print(os.path.dirname(os.path.realpath(__file__)))
 #print(get_title_text(os.path.dirname(os.path.realpath(__file__)) + "\\Toaster"))
-#print(write_html(os.path.dirname(os.path.realpath(__file__)) + "\\Potato\\Potatoe"))
+#print(write_html(os.path.dirname(os.path.realpath(__file__)) + "/"))
 #print(write_html())
 #print(get_image_count(os.path.dirname(os.path.realpath(__file__)) + "\\Potato\\Potatoe"))
